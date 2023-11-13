9a790944ebac4e77d6bae36446290c834f932e99
root <root@Vincent-GalaxyBook2Pro>
2023-11-12 20:33:01 -0800

    Add an API router

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,10 +1,8 @@
 const express = require("express");
 const router = express.Router();
+const apiRouter = require('./api');
 
-// router.get('/hello/world', function(req, res) {
-//     res.cookie('XSRF-TOKEN', req.csrfToken());
-//     res.send('Hello World!');
-// });
+router.use('/api', apiRouter);
 
 router.get("/api/csrf/restore", (req, res) => {
   const csrfToken = req.csrfToken();
