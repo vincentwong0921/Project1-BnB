88b43288dc0e706c922882bfb5b069106305254b
root <root@Vincent-GalaxyBook2Pro>
2023-11-15 22:41:27 -0800

    finished phase5

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -27,6 +27,8 @@ router.get('/', async(req, res) => {
     if(user){
         const safeUser = {
             id: user.id,
+            firstName: user.firstName,
+            lastName: user.lastName,
             email: user.email,
             username: user.username
         }
