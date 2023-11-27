2ba7518e53d3bdbd48b0734aff4e6f7fd09ee1e7
root <root@Vincent-GalaxyBook2Pro>
2023-11-27 13:36:36 -0800

    get details of a spot from an id done

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -2,7 +2,7 @@ const express = require("express");
 const bcrypt = require("bcryptjs");
 
 const { setTokenCookie, requireAuth } = require("../../utils/auth");
-const { User, Spot, Review, Image } = require("../../db/models");
+const { User, Spot, Review, Image, Booking } = require("../../db/models");
 
 const { check } = require("express-validator");
 const { handleValidationErrors } = require("../../utils/validation");
@@ -97,6 +97,57 @@ router.get('/current', requireAuth, async(req, res, next) => {
     res.json(returnData)
 })
 
+router.get('/:id', async(req, res) => {
+    const id = req.params.id
 
+    let returnData = {}
+    let spotList = []
+
+    const spot = await Spot.findOne({
+        where:{id},
+        include: [
+            {model: Review},
+            {model: Image, attributes: {exclude: ['imageableId', 'imageableType', 'createdAt', 'updatedAt']}},
+            {model: User, attributes: {exclude: ['username', 'hashedPassword', 'email', 'createdAt', 'updatedAt']}}
+        ]
+    })
+
+    if(spot){
+        spotList.push(spot.toJSON())
+
+        spotList.forEach(spot => {
+            let sum = 0
+            spot.Reviews.forEach(review => {
+                if(review.stars){
+                    sum += review.stars
+                }
+            })
+
+            let length = spot.Reviews.length
+
+            if(sum !== 0){
+                spot.avgRating = sum / spot.Reviews.length
+            } else{
+                spot.avgRating = 0
+            }
+
+            delete spot.Reviews;
+
+            spot.numReviews = length
+
+        })
+
+        returnData = spotList[0]
+
+        returnData.SpotImages = returnData.Images
+        delete returnData.Images
+        returnData.Owner = returnData.User
+        delete returnData.User
+
+        return res.json(returnData)
+    } else{
+        return res.status(404).json({message: "Spot couldn't be found"})
+    }
+})
 
 module.exports = router;
