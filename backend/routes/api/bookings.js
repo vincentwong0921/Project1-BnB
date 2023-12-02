c4f98e22cdeb81246d77cf091baa0b1c5113e607
root <root@Vincent-GalaxyBook2Pro>
2023-12-02 10:09:58 -0800

    Edit a booking done!

--- backend/routes/api/bookings.js
+++ backend/routes/api/bookings.js
@@ -35,4 +35,53 @@ router.get('/current', requireAuth, async(req, res) => {
     res.json(returnData)
 })
 
+router.put('/:bookingId', requireAuth, async(req, res, next) => {
+    const bookingId = req.params.bookingId;
+    const userId = req.user.id;
+    const today = new Date().getTime();
+    const { startDate, endDate } = req.body;
+
+    const booking = await Booking.findByPk(bookingId);
+    const existingStartDate = new Date(booking.startDate).getTime();
+    const existingEndDate = new Date(booking.endDate).getTime();
+    const newStartDate = new Date(startDate).getTime()
+    const newEndDate = new Date(endDate).getTime()
+
+    if(!booking){
+        return res.status(404).json({message: "Booking couldn't be found"})
+    }
+
+    if(booking.userId !== userId){
+        return res.status(403).json({message: 'Forbidden'})
+    }
+
+    let conflict = false;
+
+    if(today >= existingEndDate){
+        return res.status(403).json({message: "Past bookings can't be modified"})
+    } else if(
+        newStartDate >= existingStartDate && newStartDate <= existingEndDate ||
+        newEndDate >= existingStartDate && newEndDate <= existingEndDate ||
+        newStartDate <= existingStartDate && newEndDate >= existingEndDate
+    ){
+        conflict = true;
+        const err = new Error("Sorry, this spot is already booked for the specified dates")
+        err.status = 403;
+        err.errors = {
+            startDate: "Start date conflicts with an existing booking",
+            endDate: "End date conflicts with an existing booking"
+        }
+        next(err)
+    }
+
+    if(conflict === true){
+        return
+    } else {
+        booking.update({
+            startDate, endDate
+        })
+        return res.json(booking)
+    }
+})
+
 module.exports = router
