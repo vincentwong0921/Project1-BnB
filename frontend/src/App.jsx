54f56215ffb6ed33e0da369806f26f63f78e4b0a
root <root@Vincent-GalaxyBook2Pro>
2024-01-11 23:28:30 -0800

    auth1 done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,49 @@
+import LoginFormPage from "./components/LoginFormPage/LoginFormPage";
+import { Outlet, RouterProvider, createBrowserRouter } from "react-router-dom";
+import { useState, useEffect, Children } from "react";
+import * as sessionActions from './store/session'
+import { useDispatch } from "react-redux";
+import SignupFormPage from "./components/SignupFormPage/SignupFormPage";
+
+const Layout = () => {
+  const [isLoaded, setIsLoaded] = useState(false)
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch(sessionActions.restoreUser()).then(() => {
+      setIsLoaded(true)
+    })
+  }, [dispatch])
+
+  return(
+    <>
+      {isLoaded && <Outlet/>}
+    </>
+  )
+}
+
+const router = createBrowserRouter([
+  {
+    element: <Layout/>,
+    children: [
+      {
+        path: "/",
+        element: <h1>Welcome!</h1>,
+      },
+      {
+        path: "/login",
+        element: <LoginFormPage />,
+      },
+      {
+        path: '/signup',
+        element: <SignupFormPage/>
+      }
+    ]
+  },
+]);
+
 function App() {
-  return <h1> Hello from App </h1>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
---
+++ frontend/src/store/session.js
@@ -0,0 +1,84 @@
+import { csrfFetch } from "./csrf"
+
+const SET_USER = 'user/SET_USER'
+const REMOVE_USER = 'user/REMOVE_USER'
+
+/* actions */
+export const setUser = user => ({
+    type: SET_USER,
+    user
+})
+
+export const removeUser = () => ({
+    type: REMOVE_USER,
+})
+
+
+/* thunks */
+export const loginUser = (user) => async (dispatch) => {
+    const { credential, password } = user
+
+    const response = await csrfFetch('/api/session', {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({
+            credential,
+            password
+        })
+    })
+
+    const data = await response.json();
+    dispatch(setUser(data.user))
+    return response
+}
+
+export const restoreUser = () => async (dispatch) => {
+    const response = await csrfFetch('/api/session')
+    const data = await response.json()
+    dispatch(setUser(data.user))
+    return response
+}
+
+export const signUpUser = user => async (dispatch) => {
+    const {username, firstName, lastName, email, password} = user
+
+    const response = await csrfFetch('/api/users', {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({
+            username, firstName, lastName, email, password
+        })
+    })
+
+    const data = await response.json()
+    console.log(data)
+    dispatch(setUser(data.user))
+    return response
+}
+
+/* selectors */
+
+
+
+
+/* reducer */
+
+const initialState = { user: null }
+
+const sessionReducer = (state = initialState, action) => {
+    switch (action.type){
+        case SET_USER: {
+            return {
+                ...state,
+                user: action.user
+            }
+        }
+        case REMOVE_USER: {
+            return {...state, user: null}
+        }
+        default:
+            return state;
+    }
+}
+
+export default sessionReducer
